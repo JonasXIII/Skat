3e5279f603204dc2802c3dfbbcac8fdc01544ac6
JonasXIII <jonasloeser@gmail.com>
2023-03-30 09:39:40 -0700

    adding ui

--- client.py
+++ client.py
@@ -5,3 +5,7 @@ def getBiddingOkay(player, biddingNum):
             return True
         if answer in {"n","N","no", "No"}:
             return False
+        
+def getSuitToPlay(player):
+    while True:
+        answer = input(f"{player.name} what would you like trump to be? {{1:Blue}} {{2:Green}} {{3:Red}} {{4:Yellow}} {{5:Grand}} {{6:Null}}")
