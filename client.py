f5d11409c35940aa9533aa88b632616abbcbb5c3
JonasXIII <jonasloeser@gmail.com>
2023-04-07 11:31:41 -0700

    adding more comunication between server and client

--- client.py
+++ client.py
@@ -1,17 +1,24 @@
 import socket
 
-
+name = ""
 
 def main():
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect(("localhost", 8000))
     
-    recv(conn)
+    connected(conn)
     recv(conn)
     recv(conn)
 
 
 
+
+def bidding():
+    pass
+
+def connected(conn):
+    name = conn.recv(1024).decode()
+    print(f"Connection to server succesful, you are {name}.")
 
 def recv(conn):
     data = conn.recv(1024).decode()
--- server.py
+++ server.py
@@ -8,11 +8,11 @@ def main():
     s.listen()
     print("Waiting for connections...")
     conn1, addr1 = s.accept()
-    connected(conn1,addr1)
+    connected(conn1,addr1, "Silke")
     conn2, addr2 = s.accept()
-    connected(conn2,addr2)
+    connected(conn2,addr2, "Jork")
     conn3, addr3 = s.accept()
-    connected(conn3,addr3)
+    connected(conn3,addr3, "Jonas")
     
     tellAll(conn1,conn2,conn3,"Wellcome to Skat, three people have successfully connected.")
     deal(allCards, player1, player2,player3)
@@ -30,9 +30,11 @@ def sendHand(conn, player):
     conn.sendall(msg.encode())
 
 
-def connected(conn, addr):
+def connected(conn, addr, name):
     print(f"Player connected from {addr}")
-    conn.sendall(b"Connected to server")
+    msg = name #+ ", you have succesfully conneted to server."
+    conn.sendall(msg.encode())
+    #conn.sendall(b"Connected to server, you are ")
 
 def tellAll(conn1,conn2,conn3,msg):
     conn1.sendall(msg.encode())
